443144031967c54206c2f6989a34a9e5c549f816
Tobias Bell <tobias.bell@gmail.com>
2020-11-29 23:39:18 +0100

    Some refactoring

--- src/test/java/de/tobiasbell/aoc_2015/day8/Day8Test.java
+++ src/test/java/de/tobiasbell/aoc_2015/day8/Day8Test.java
@@ -11,13 +11,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class Day8Test {
 
-    @ParameterizedTest(name = "String {0} has length {1} and in memory length {2}")
+    @ParameterizedTest(name = "{index}: {0} length {1} in memory length {2}")
     @CsvSource({
-            "\"\", 2, 0",
-            "\"abc\", 5, 3",
+            "\"\",            2, 0",
+            "\"abc\",         5, 3",
             "\"aaa\\\"aaa\", 10, 7",
-            "\"\\\", 3, 1",
-            "\"\\x27\", 6, 1"
+            "\"\\\",          3, 1",
+            "\"\\x27\",       6, 1"
     })
     void inMemoryString(final String input, int length, int inMemoryLength) {
         assertThat(input.length()).isEqualTo(length);
